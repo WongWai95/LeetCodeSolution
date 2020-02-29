d7cb6bd8be29f337a86efad292d19723262cb8de
WongWai95 <56151936+WongWai95@users.noreply.github.com>
2020-02-29 09:30:53 +0800

    add new solution

---
+++ 153. 寻找旋转排序数组中的最小值/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = left + (right - left) // 2
+            # print(nums[mid])
+            if nums[mid] <= nums[right]:
+                right = mid
+            elif nums[mid] >= nums[left]:
+                left = mid + 1
+        return nums[left]
